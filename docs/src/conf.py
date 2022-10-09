292826d43eb3d38458bede5412cf8c74fe170899
i0bs <41456914+i0bs@users.noreply.github.com>
2022-10-08 22:47:51 -0400

    feat: init

---
+++ docs/src/conf.py
@@ -0,0 +1,18 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'DCS Beginners Guide to Systems Coding'
+copyright = '2022, vapourlabs'
+author = 'vapourlabs'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'press'
+html_static_path = ['_static']
---
+++ docs/src/index.rst
@@ -0,0 +1,20 @@
+.. BGSC documentation master file, created by
+   sphinx-quickstart on Sat Oct  8 22:41:54 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to BGSC's documentation!
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
